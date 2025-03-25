5451404a10033209095a3b95ac3f0de2ab6fe517
DevSw <64429051+devsw-prayas@users.noreply.github.com>
2025-03-25 19:58:16 +0530

    Added Auto Project Generation

---
+++ src/SpectraCore/src/Private/S_uint4.cpp
@@ -0,0 +1,37 @@
+#include "S_uint4.h"
+
+namespace spectra::core::math {
+	S_uint4::S_uint4(int value) {
+		bits = BIT_MASK_4(value);
+	}
+	int S_uint4::value() const {
+		return bits;
+	}
+	void S_uint4::print() const {
+		instrumentation::Instrumentation::logMath(instrumentation::E_LogLevel::INFO, "spectra::core::math", "S_uint4", "Value: {}", value());
+	}
+
+	S_uint4::S_uint4(const S_uint4& other) = default;
+
+	S_uint4::S_uint4(S_uint4&& other) noexcept : bits(other.bits) {
+		other.bits = 0;
+	}
+
+	S_uint4& S_uint4::operator=(const S_uint4& other) {
+		if (this != &other) {
+			bits = other.bits;
+		}
+		return *this;
+	}
+
+	S_uint4& S_uint4::operator=(S_uint4&& other) noexcept {
+		if (this != &other) {
+			bits = other.bits;
+			other.bits = 0;
+		}
+		return *this;
+	}
+
+
+
+}
