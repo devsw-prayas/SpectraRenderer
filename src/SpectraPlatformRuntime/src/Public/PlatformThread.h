42d461f4c6f4093893aa714bc66afdff030b8ae9
DevSw <64429051+devsw-prayas@users.noreply.github.com>
2026-01-15 21:00:56 +0530

    Small updates

--- src/SpectraPlatformRuntime/src/Public/PlatformThread.h
+++ src/SpectraPlatformRuntime/src/Public/PlatformThread.h
@@ -4,9 +4,32 @@
 
 #include <cstdint>
 
-namespace Spectra::Runtime::Platform::Thread {
+namespace Spectra::Platform::Runtime::Thread {
 
 	class RUNTIME PlatformThread final {
+		// Handle Ops
+		static ThreadHandle createThread(
+			const ThreadLaunchDesc& ro_LaunchDesc, const ThreadLaunchExecDesc& ro_ExecDesc) noexcept;
+		static bool detachThread(ThreadHandle v_Handle) noexcept;
+		static bool closeHandle(ThreadHandle v_Handle) noexcept;
+		static ThreadHandle duplicateHandle(ThreadHandle v_handle) noexcept;
+
+		static bool isAlive(ThreadHandle v_Handle) noexcept;
+		static ProcessorIdx getThreadID(ThreadHandle v_Handle) noexcept;
+
+		static bool suspendThread(ThreadHandle v_Handle) noexcept;
+		static bool resumeThread(ThreadHandle v_Handle) noexcept;
+		static bool terminateThread(ThreadHandle v_Handle) noexcept;
+		static bool joinThread(ThreadHandle v_Handle) noexcept;
+
+		static void waitOnAddress(ParkHandle& ro_Permit) noexcept;
+		static void wakeOnAddress(ParkHandle& ro_Permit) noexcept;
+		static void wakeAllOnAddress(ParkHandle& ro_Permit) noexcept;
+
+		template<typename T>
+		static void waitOnAddressFor(ParkHandle& ro_Handle, T&& u_Duration) noexcept {
+			
+		}
 
 
 		friend struct ThreadHandle;
